13ef0a5d10fd961e0116c693bd761c6a03288b1f
Felipe Bell Marques <felipe.bellmar@gmail.com>
2019-06-13 22:51:04 -0300

    dev

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,24 +1,25 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
-  if(deferredPrompt){
+  if (deferredPrompt) {
     deferredPrompt.prompt();
 
-    deferredPrompt.userchoice.then(function(choiceResult){
-      console.log('choiceResult.outcome');
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
 
-      if(choiceResult.outcome === 'dismissed'){
+      if (choiceResult.outcome === 'dismissed') {
         console.log('User cancelled installation');
-      }else{
+      } else {
         console.log('User added to home screen');
       }
     });
 
     deferredPrompt = null;
-  } 
+  }
 }
 
 function closeCreatePostModal() {
@@ -31,12 +32,12 @@ closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
 function createCard() {
   var cardWrapper = document.createElement('div');
-  cardWrapper.className = 'shared-momentcard mdl-card mdl-shadow--2dp';
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
   cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = "100px";
+  cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.className = 'mdl-card__title-text';
@@ -48,12 +49,13 @@ function createCard() {
   cardSupportingText.style.textAlign = 'center';
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
-  saredMomentsArea.appendChild(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
 }
 
 fetch('https://httpbin.org/get')
-  .then(function(res){
+  .then(function(res) {
     return res.json();
-  }).then(function(data){
-    createCard;
+  })
+  .then(function(data) {
+    createCard();
   });
