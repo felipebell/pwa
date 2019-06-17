47c649deab8ac467b703589a5338023bd430228b
Felipe Bell <fbmarques@stefanini.com>
2019-06-17 11:44:24 -0300

    dev

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,5 @@
 
 var deferredPrompt;
-var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -8,7 +7,7 @@ if (!window.Promise) {
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('service-worker.js')
+    .register('/sw.js')
     .then(function () {
       console.log('Service worker registered!');
     })
@@ -24,90 +23,74 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
-function displayConfirmNotification() {
-  if ('serviceWorker' in navigator) {
-    var options = {
-      body: 'You successfully subscribed to our Notification service!',
-      icon: 'src/images/icons/app-icon-96x96.png',
-      image: 'src/images/sf-boat.jpg',
-      dir: 'ltr',
-      lang: 'en-US', // BCP 47,
-      vibrate: [100, 50, 200],
-      badge: 'src/images/icons/app-icon-96x96.png',
-      tag: 'confirm-notification',
-      renotify: true,
-      actions: [
-        { action: 'confirm', title: 'Okay', icon: 'src/images/icons/app-icon-96x96.png' },
-        { action: 'cancel', title: 'Cancel', icon: 'src/images/icons/app-icon-96x96.png' }
-      ]
-    };
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    //resolve('This is executed once the timer is done!');
+    reject({code: 500, message: 'An error occurred!'});
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+});
 
-    navigator.serviceWorker.ready
-      .then(function(swreg) {
-        swreg.showNotification('Successfully subscribed!', options);
-      });
-  }
-}
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
 
-function configurePushSub() {
-  if (!('serviceWorker' in navigator)) {
-    return;
-  }
+xhr.onload = function() {
+  console.log(xhr.response);
+};
 
-  var reg;
-  navigator.serviceWorker.ready
-    .then(function(swreg) {
-      reg = swreg;
-      return swreg.pushManager.getSubscription();
-    })
-    .then(function(sub) {
-      if (sub === null) {
-        // Create a new subscription
-        var vapidPublicKey = 'BKapuZ3XLgt9UZhuEkodCrtnfBo9Smo-w1YXCIH8YidjHOFAU6XHpEnXefbuYslZY9vtlEnOAmU7Mc-kWh4gfmE';
-        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
-        return reg.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: convertedVapidPublicKey
-        });
-      } else {
-        // We have a subscription
-      }
-    })
-    .then(function(newSub) {
-      return fetch('https://pwagram-99adf.firebaseio.com/subscriptions.json', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: JSON.stringify(newSub)
-      })
-    })
-    .then(function(res) {
-      if (res.ok) {
-        displayConfirmNotification();
-      }
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-}
+xhr.onerror = function() {
+  console.log('Error!');
+};
 
-function askForNotificationPermission() {
-  Notification.requestPermission(function(result) {
-    console.log('User Choice', result);
-    if (result !== 'granted') {
-      console.log('No notification permission granted!');
-    } else {
-      configurePushSub();
-      // displayConfirmNotification();
-    }
+xhr.send();
+
+fetch('https://httpbin.org/ip')
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
-}
 
-if ('Notification' in window && 'serviceWorker' in navigator) {
-  for (var i = 0; i < enableNotificationsButtons.length; i++) {
-    enableNotificationsButtons[i].style.display = 'inline-block';
-    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
-  }
-}
+fetch('https://httpbin.org/post', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  mode: 'cors',
+  body: JSON.stringify({message: 'Does this work?'})
+})
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
+
+promise.then(function(text) {
+  return text;
+}).then(function(newText) {
+  console.log(newText);
+}).catch(function(err) {
+  console.log(err.code, err.message);
+});
+
+console.log('This is executed right after setTimeout()');
