dddce9ab6d5ca64753fe53f3d6b9284458b78e9b
Felipe Bell <fbmarques@stefanini.com>
2019-06-18 11:13:30 -0300

    dev

--- public/sw.js
+++ public/sw.js
@@ -4,22 +4,20 @@ self.addEventListener('install', function(event) {
     caches.open('static')
       .then(function(cache){
         console.log('[Service Worker] Precaching App Shell');
-        cache.add('../public/')
-        cache.addAll([
-          
-          'index.html',
-          'src/js/app.js',
-          'src/js/feed.js',
-          'src/js/promise.js',
-          'src/js/fetch.js',
-          'src/js/material.min.js',
-          'src/css/app.css',
-          'src/css/feed.css',
-          'src/image/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.add('../public/');
+        cache.add('index.html');
+        cache.add('src/js/app.js');
+        cache.add('src/js/feed.js');
+        cache.add('src/js/promise.js');
+        cache.add('src/js/fetch.js');
+        cache.add('src/js/material.min.js');
+        cache.add('src/css/app.css');
+        cache.add('src/css/feed.css');
+        cache.add('src/image/main-image.jpg');
+        cache.add('https://fonts.googleapis.com/css?family=Roboto:400,700');
+        cache.add('https://fonts.googleapis.com/icon?family=Material+Icons');
+        cache.add('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css');
+        
         
       })
   )
