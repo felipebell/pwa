f7367c54bf683fe361e122657029ca607d47ff0e
Felipe Bell <fbmarques@stefanini.com>
2019-06-24 13:55:57 -0300

    dev

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache){
         console.log('[Service Worker] Precaching App Shell');
         cache.add('../public/');
@@ -27,8 +30,9 @@ self.addEventListener('activate', function(event) {
     caches.keys()
       .then(function(keyList){
         return Promise(keyList.map(function(key){
-          if(key !== 'static-v2' && key !== 'dynamic'){
+          if(key !== 'static-v2' && key !== CACHE_DYNAMIC_NAME){
             console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
           }
         }));
       })
@@ -45,7 +49,7 @@ self.addEventListener('fetch', function(event) {
         }else{
           return fetch(event.request)
           .then(function(res){
-            caches.open('dynamic')
+            caches.open(CACHE_DYNAMIC_NAME)
             .then(function(cache){
               cache.put(event.request.url, res.clone());
               return res;
