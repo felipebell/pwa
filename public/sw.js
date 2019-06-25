ca12c27adf3509c77626aa863a733c2444bacfa3
Felipe Bell <fbmarques@stefanini.com>
2019-06-25 10:58:04 -0300

    dev

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_NAME = 'static-v13';
+var CACHE_STATIC_NAME = 'static-v14';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 
 self.addEventListener('install', function(event) {
@@ -69,26 +69,13 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response){
-        if(response){
-          return response;
-        }else{
-          return fetch(event.request)
+    caches.open(CACHE_DYNAMIC_NAME)
+      .then(function(cache){
+        return fetch(event.request)
           .then(function(res){
-            caches.open(CACHE_DYNAMIC_NAME)
-            .then(function(cache){
-              cache.put(event.request.url, res.clone());
-              return res;
-            })
-          })
-          .catch(function(err){
-            return caches.open(CACHE_STATIC_NAME)
-              .then(function(cache){
-                return cache.match('offline.html');
-              });
+            cache.put(event.request, res.clone());
+            return res;
           });
-        }
       })
   );
 });
